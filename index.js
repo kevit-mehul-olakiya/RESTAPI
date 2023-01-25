fe9d77c7ac7f624ff8a5a7dbcc3c5fa1a748d442
kevit-mehul-olakiya <119293929+kevit-mehul-olakiya@users.noreply.github.com>
2023-01-25 11:11:44 +0530

    all done

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.get('/user-posts-comments', async (req, res) => {
+app.get('/user', async (req, res) => {
   const { username } = req.query;
 
-  // Fetch the list of users
-  const usersResponse = await fetch('https://jsonplaceholder.typicode.com/users');
+// fetch user
+  const usersResponse = await fetch('https://jsonplaceholder.typicode.com/users')
   const users = await usersResponse.json();
-
-  // Filter the list of users to find the user with the matching username
   const user = users.filter((user) => user.username === username)[0];
  
-  
-  // Fetch the list of posts
-  const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const posts = await postsResponse.json();
-  
-  // Filter the list of posts to find the posts that have a userId field that matches the user's id
+  // fetch post
+   const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await postsResponse.json();
   const userPosts = posts.filter((post) => post.userId === user.id);
-  // Fetch the list of comments
+
+// fetch comment
   const commentsResponse = await fetch('https://jsonplaceholder.typicode.com/comments');
   const comments = await commentsResponse.json();
-
-  // Filter the list of comments to find the comments that have a postId field that matches the post's id
   const userPostsComments = comments.filter((comment) => userPosts.find((post)=> post.id === comment.postId));
  
   res.json({ 
--- subtask2.js
+++ subtask2.js
@@ -1,5 +1,47 @@
 const express = require('express');
 const app = express();
 
+app.get('/comments-to-user',async(req,res)=>{
+    const {email,name} = req.query;
 
+    // fetch comments
+    const commentsResponse = await fetch('https://jsonplaceholder.typicode.com/comments');
+    const comments = await commentsResponse.json();
+
+    const comment = comments.filter((comment) => comment.email === email && comment.name === name)
+
+    if(comment === undefined){
+      res.status(400).json( `${email} and ${name} is not found`)
+    }
+    // console.log(comment);
+    
+    // fetch post
+    const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await postsResponse.json();
+
+    const post = posts.filter((post)=>comment.find((comment)=>post.id === comment.postId))
+    // console.log(post);
+    if(post === undefined){
+        res.status(400).json( `${email} and ${name} is not found`)
+      }
+    
+// fetch user
+    const usersResponse = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await usersResponse.json();
+
+    const user = users.filter((user)=>post.find((post)=>post.userId === user.id))[0]
+    if(user === undefined){
+        res.status(400).json( {
+            error:`${email} and ${name} is not found`
+        })
+      }
+    
+// console.log(user);
+
+    res.json({
+        username:user.username,
+        name:user.name,
+        email:user.email
+    })
+})
 app.listen(3000, () => console.log('Server started on port 3000'));
