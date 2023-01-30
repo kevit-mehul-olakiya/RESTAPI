db33f11f7db87c7bd69c57a662ff612e1970c7db
kevit-mehul-olakiya <119293929+kevit-mehul-olakiya@users.noreply.github.com>
2023-01-30 12:36:05 +0530

    send error

--- index.js
+++ index.js
@@ -1,31 +1,130 @@
 const express = require('express');
 const app = express();
-
+app.use(express.json())
 app.get('/user', async (req, res) => {
   const { username } = req.query;
 
-// fetch user
+try {
+    // fetch user
   const usersResponse = await fetch('https://jsonplaceholder.typicode.com/users')
   const users = await usersResponse.json();
-  const user = users.filter((user) => user.username === username)[0];
- 
+
   // fetch post
    const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
     const posts = await postsResponse.json();
-  const userPosts = posts.filter((post) => post.userId === user.id);
 
 // fetch comment
   const commentsResponse = await fetch('https://jsonplaceholder.typicode.com/comments');
   const comments = await commentsResponse.json();
-  const userPostsComments = comments.filter((comment) => userPosts.find((post)=> post.id === comment.postId));
  
-  res.json({ 
-    username: user.username,
-    name:user.name,
-    email:user.email,
-    city:user.address.city,
-    userPosts: userPosts, 
-    userPostsComments: userPostsComments });
-});
+ 
+  let postToComment = {}
+  let userToPost = {}
+  let user = {}
+  for(let i=0;i<comments.length;i++){
+      if(!postToComment[comments[i].postId]){
+          postToComment[comments[i].postId] = []
+          postToComment[comments[i].postId].push( {
+              name:comments[i].name,
+              body:comments[i].body
+          })
+      }
+      else{
+          postToComment[comments[i].postId].push( {
+              name:comments[i].name,
+              body:comments[i].body
+          })
+      }
+  }
+
+  for (let i = 0; i <posts.length; i++) {
+      if(!userToPost[posts[i].userId]){
+
+          userToPost[posts[i].userId] = []
+
+          userToPost[posts[i].userId].push({
+
+              title:posts[i].title,
+              comment:postToComment[posts[i].id]
+          })
+      }else{
+          userToPost[posts[i].userId].push( {
+
+              title:posts[i].title,
+              comment:postToComment[posts[i].id]
+          })
+      }
+  }
+  for (let i = 0; i <users.length; i++) {
+      if(username === users[i].username){
+        user = {
+            userId:users[i].id,
+            name:users[i].name,
+            username:users[i].username,
+            email:users[i].email,
+            city:users[i].address.city,
+            post : userToPost[users[i].id]
+        }
+      }
+  }
+  if (username !== user.username) {
+    return   res.status(400).send({error:`user not found`})  
+  }
+  return res.send({ 
+    user:user
+ });
+  
+} catch (error) {
+    res.status(500).send(error)
+}
+})
+//<------------------------------------------------- subtask 2 ------------------------------------->
+app.get('/comments-to-user', async (req, res) => {
+    const { email, name } = req.query;
+
+ try {
+       // fetch comments
+       const commentsResponse = await fetch('https://jsonplaceholder.typicode.com/comments');
+       const comments = await commentsResponse.json();
+   
+       const comment = comments.filter((comment) => comment.email === email && comment.name === name)
+   
+       if (!comment) {
+        return   res.status(404).send(`${email} and ${name} is not found`)
+       }
+       // console.log(comment);
+   
+       // fetch post
+       const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
+       const posts = await postsResponse.json();
+   
+       const post = posts.filter((post) => comment.find((comment) => post.id === comment.postId))
+       // console.log(post);
+       if (!post) {
+        return   res.status(404).send(`${email} and ${name} is not found`)
+       }
+   
+       // fetch user
+       const usersResponse = await fetch('https://jsonplaceholder.typicode.com/users')
+       const users = await usersResponse.json();
+   
+       const user = users.filter((user) => post.find((post) => post.userId === user.id))[0]
+       if (!user) {
+           return res.status(400).send({
+               error: `${email} and ${name} is not found`
+           })
+       }
+   
+       // console.log(user);
+   
+       res.json({
+           username: user.username,
+           name: user.name,
+           email: user.email
+       })
+ } catch (error) {
+    res.status(500).send(error)
+ }
+})
 
 app.listen(3000, () => console.log('Server started on port 3000'));
--- subtask2.js
+++ subtask2.js
@@ -1,47 +1,51 @@
 const express = require('express');
 const app = express();
 
-app.get('/comments-to-user',async(req,res)=>{
-    const {email,name} = req.query;
+app.get('/comments-to-user', async (req, res) => {
+    const { email, name } = req.query;
 
-    // fetch comments
-    const commentsResponse = await fetch('https://jsonplaceholder.typicode.com/comments');
-    const comments = await commentsResponse.json();
-
-    const comment = comments.filter((comment) => comment.email === email && comment.name === name)
-
-    if(comment === undefined){
-      res.status(400).json( `${email} and ${name} is not found`)
-    }
-    // console.log(comment);
-    
-    // fetch post
-    const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const posts = await postsResponse.json();
-
-    const post = posts.filter((post)=>comment.find((comment)=>post.id === comment.postId))
-    // console.log(post);
-    if(post === undefined){
-        res.status(400).json( `${email} and ${name} is not found`)
-      }
-    
-// fetch user
-    const usersResponse = await fetch('https://jsonplaceholder.typicode.com/users')
-    const users = await usersResponse.json();
-
-    const user = users.filter((user)=>post.find((post)=>post.userId === user.id))[0]
-    if(user === undefined){
-        res.status(400).json( {
-            error:`${email} and ${name} is not found`
-        })
-      }
-    
-// console.log(user);
-
-    res.json({
-        username:user.username,
-        name:user.name,
-        email:user.email
-    })
+ try {
+       // fetch comments
+       const commentsResponse = await fetch('https://jsonplaceholder.typicode.com/comments');
+       const comments = await commentsResponse.json();
+   
+       const comment = comments.filter((comment) => comment.email === email && comment.name === name)
+   
+       if (comment === undefined) {
+           res.status(404).send(`${email} and ${name} is not found`)
+       }
+       // console.log(comment);
+   
+       // fetch post
+       const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
+       const posts = await postsResponse.json();
+   
+       const post = posts.filter((post) => comment.find((comment) => post.id === comment.postId))
+       // console.log(post);
+       if (post === undefined) {
+           res.status(404).send(`${email} and ${name} is not found`)
+       }
+   
+       // fetch user
+       const usersResponse = await fetch('https://jsonplaceholder.typicode.com/users')
+       const users = await usersResponse.json();
+   
+       const user = users.filter((user) => post.find((post) => post.userId === user.id))[0]
+       if (user === undefined) {
+           res.status(400).send({
+               error: `${email} and ${name} is not found`
+           })
+       }
+   
+       // console.log(user);
+   
+       res.json({
+           username: user.username,
+           name: user.name,
+           email: user.email
+       })
+ } catch (error) {
+    res.status(500).send(error)
+ }
 })
 app.listen(3000, () => console.log('Server started on port 3000'));
